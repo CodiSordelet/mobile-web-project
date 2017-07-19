3e6a2c4aacb5b862c01e704f161f89988a8e1e78
jsninja1994 <codisordelet@Codis-MacBook-Pro.local>
2017-07-19 01:20:31 -0400

    First commit
    First commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+  var compilation = function (url, tplId, anchor) {
+       $.getJSON(url, function(data) {
+                var template = $(tplId).html();
+                var stone = Handlebars.compile(template)(data);
+                $(anchor).append(stone);
+       });
+    }
+      //4b.function firing
+      compilation('../jobs.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
+
+
+$.get("../jobs.json", function(data) {
+
+    console.log(data);
+
+});
+
+
+
+
+}); // end ready
